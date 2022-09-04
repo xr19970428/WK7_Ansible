7bcd0feca9a84d6fe5604ae3be5444a3a3379d76
William Dong <williamdong@Williams-MacBook-Pro.local>
2022-09-04 15:36:12 +1000

    update nginx homepage for ansible instance

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -51,6 +51,31 @@ resource "aws_instance" "packer-ansible" {
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.deployer.key_name}"
 
+  user_data = <<EOD
+    #!/bin/bash
+    systemctl start nginx.service
+    systemctl enable nginx.service
+    cat <<EOF > /var/www/html/index.html
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Welcome to Jiangren Devops!</title>
+    <style>
+        body {
+            width: 35em;
+            margin: 0 auto;
+            font-family: Tahoma, Verdana, Arial, sans-serif;
+        }
+    </style>
+    </head>
+    <body>
+    <h1>Welcome to Jiangren Devops!</h1>
+    <p>Hello from $(hostname -f)</p>
+    </body>
+    </html>
+    EOF
+EOD
+
   tags = {
     Name = "Packer-Ansible"
   }
