a2cbe77883ee1417365a0590bdde7ea9a36f5e66
William Dong <williamdong@Williams-MacBook-Pro.local>
2022-09-05 22:52:16 +1000

    updated destroy ami

--- terraform/aws/del_ami.sh
+++ terraform/aws/del_ami.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 AMIList=($(aws ec2 describe-images --owners=self --filters "Name=name,Values=*packer-demo*" --query 'Images[*].[ImageId]' --output text))
+echo $AMIList
 for i in $AMIList
 do
     echo "Destroying AMI ID: $i"
