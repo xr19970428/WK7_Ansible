90c2c6f72b301bbeea3d8af9e5d9dec27f6ed35a
William Dong <williamdong@Williams-MacBook-Pro.local>
2022-09-05 15:51:44 +1000

    fixed az error

--- terraform/az/outputs.tf
+++ terraform/az/outputs.tf
@@ -1,12 +1,7 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.example.name
 }
 
 output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
-}
-
-output "tls_private_key" {
-  value     = tls_private_key.example_ssh.private_key_pem
-  sensitive = true
+  value = azurerm_linux_virtual_machine.example.public_ip_address
 }
